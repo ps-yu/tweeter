f04963bbd30ddf18beaf57c4b9dc23ba4ecf2d8c
ps-yu <pankajsharma12540@gmail.com>
2022-12-31 22:07:14 -0500

    Change the font to red when the user exceeds the text limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,13 @@
+//Function responsible for counting character
 $(document).ready(() => {
   $('textarea').on('input', function(){
     let target = $(this).parent().find('.counter')
     target.text( 140 - $(this).val().length)
-    if (target.text > 0){
-      target.css ({'color' : '#e60505'});
+    console.log($(this).val().length)
+    if ($(this).val().length > 140){
+      target.css ({'color' : '#f7490f'});
+    } else {
+      target.css({'color': '#545149'})
     }
 })
 });
