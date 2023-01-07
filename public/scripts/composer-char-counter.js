94cd7095e0963d273be0abcf233c113826b3f728
ps-yu <pankajsharma12540@gmail.com>
2023-01-07 16:54:10 -0500

    Add a main container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,12 @@
-//Function responsible for counting character
+/**
+ * Target: <form>/<textarea>/#tweet-text
+ * Changes the value of class("counter") on keyboard input
+ * Changes the color of the text when the text exceeds the limit.
+ */
 $(document).ready(() => {
-  $('textarea').on('input', function(){
+  $('#tweet-text').on('input', function(){
     let target = $(this).parent().find('.counter')
     target.text( 140 - $(this).val().length)
-    console.log($(this).val().length)
     if ($(this).val().length > 140){
       target.css ({'color' : '#f7490f'});
     } else {
@@ -11,4 +14,3 @@ $(document).ready(() => {
     }
 })
 });
-
